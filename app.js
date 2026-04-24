19895195a9e8abc427f0cd4c893d0504e5312800
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2026-04-24 07:24:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,13 @@ function add(a, b) {
     return a + b;
 }
 
+function multiply(a, b) {
+    if (a === 0 || b === 0) {
+        return 0;
+    }
+    return a * b;
+}
+
 // ✅ EXPORT BOTH
 module.exports = { app, add };
 
