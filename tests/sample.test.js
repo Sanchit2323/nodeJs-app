6575d97405bd10de9e7904387f7053b3f306401c
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2026-04-24 05:51:18 +0530

    Update sample.test.js

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,3 +1,13 @@
-test('sample test', () => {
-  expect(1 + 1).toBe(2);
+const { add } = require('../app');
+
+test('addition positive numbers', () => {
+  expect(add(2, 2)).toBe(4);
+});
+
+test('addition negative numbers', () => {
+  expect(add(-2, -2)).toBe(-4);
+});
+
+test('addition mixed numbers', () => {
+  expect(add(5, -2)).toBe(3);
 });
